6cb291812155ecd83d34ae0dcb3aa5719ace4191
Jonathan Fuerst <jf@jofu.org>
2025-07-14 19:18:47 +0200

    fixed typo

--- run_all_slurm_large.sh
+++ run_all_slurm_large.sh
@@ -4,13 +4,13 @@
 sbatch --partition=gpu --nodelist=sanfrancisco --time=4320 ./run_benchmark_slurm.submit config-large.yaml
 
 # A100
-sbatch --partition=gpu --nodelist=sacramento --time=4320 ./run_benchmark_slurm.submit config-large.yml
+sbatch --partition=gpu --nodelist=sacramento --time=4320 ./run_benchmark_slurm.submit config-large.yaml
 
 # H100
-sbatch --partition=gpu_top --nodelist=sanjose --time=1440 ./run_benchmark_slurm.submit config-large.yml
+sbatch --partition=gpu_top --nodelist=sanjose --time=1440 ./run_benchmark_slurm.submit config-large.yaml
 
 # H200
-sbatch --partition=gpu_top --nodelist=trinity --time=1440 ./run_benchmark_slurm.submit config-large.yml
+sbatch --partition=gpu_top --nodelist=trinity --time=1440 ./run_benchmark_slurm.submit config-large.yaml
 
 # L40
-sbatch --partition=gpu_top --nodelist=fresko --gres=gpu:l40spcie:1 --time=1440 ./run_benchmark_slurm.submit config-large.yml
+sbatch --partition=gpu_top --nodelist=fresko --gres=gpu:l40spcie:1 --time=1440 ./run_benchmark_slurm.submit config-large.yaml
--- run_all_slurm_small.sh
+++ run_all_slurm_small.sh
@@ -4,13 +4,13 @@
 sbatch --partition=gpu --nodelist=sanfrancisco --time=4320 ./run_benchmark_slurm.submit config-small.yaml
 
 # A100
-sbatch --partition=gpu --nodelist=sacramento --time=4320 ./run_benchmark_slurm.submit config-small.yml
+sbatch --partition=gpu --nodelist=sacramento --time=4320 ./run_benchmark_slurm.submit config-small.yaml
 
 # H100
-sbatch --partition=gpu_top --nodelist=sanjose --time=1440 ./run_benchmark_slurm.submit config-small.yml
+sbatch --partition=gpu_top --nodelist=sanjose --time=1440 ./run_benchmark_slurm.submit config-small.yaml
 
 # H200
-sbatch --partition=gpu_top --nodelist=trinity --time=1440 ./run_benchmark_slurm.submit config-small.yml
+sbatch --partition=gpu_top --nodelist=trinity --time=1440 ./run_benchmark_slurm.submit config-small.yaml
 
 # L40
-sbatch --partition=gpu_top --nodelist=fresko --gres=gpu:l40spcie:1 --time=1440 ./run_benchmark_slurm.submit config-small.yml
+sbatch --partition=gpu_top --nodelist=fresko --gres=gpu:l40spcie:1 --time=1440 ./run_benchmark_slurm.submit config-small.yaml
