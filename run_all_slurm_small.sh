d5ef55b7bcce22b9d294164f1fc3b1f602d3f7ad
Jonathan Fuerst <jf@jofu.org>
2025-07-14 11:24:49 -0400

    run all for large and small

---
+++ run_all_slurm_small.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# V100
+sbatch --partition=gpu --nodelist=sanfrancisco --time=4320 ./run_benchmark_slurm.submit config-small.yaml
+
+# A100
+sbatch --partition=gpu --nodelist=sacramento --time=4320 ./run_benchmark_slurm.submit config-small.yml
+
+# H100
+sbatch --partition=gpu_top --nodelist=sanjose --time=1440 ./run_benchmark_slurm.submit config-small.yml
+
+# H200
+sbatch --partition=gpu_top --nodelist=trinity --time=1440 ./run_benchmark_slurm.submit config-small.yml
+
+# L40
+sbatch --partition=gpu_top --nodelist=fresko --gres=gpu:l40spcie:1 --time=1440 ./run_benchmark_slurm.submit config-small.yml
